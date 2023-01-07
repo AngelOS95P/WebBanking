3e1124578b98363c45f8bbf223efd4b86af87e07
AngelOS95P <andres95_p@hotmail.com>
2023-01-06 19:57:31 -0500

    1

---
+++ BankingLibrary/Operaciones.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingLibrary
+{
+    public static class Operaciones
+    {
+        public static string cuenta { get; set; }
+        public static double? saldo { get; set; }
+        public static bool abierta { get; set; } = false;
+        public static int? movimientos { get; set; }
+        public static void AperturaCuenta()
+        {
+            cuenta = "1001";
+            saldo = 0;
+            abierta = true;
+            movimientos = 0;
+        }
+        public static void Deposito(double valor)
+        {
+            saldo += valor;
+            movimientos++;
+        }
+        public static void Retiro(double valor)
+        {
+            if (saldo < valor)
+                throw new ArgumentException("el valor de retiro sobregira la cuenta");
+            else
+            {
+                saldo -= valor;
+                movimientos++;
+            }
+        }
+
+        public static void BadCoding(double tasa)
+        {
+            int sinuso;
+
+            //for (int i = 0; i < length; i++)
+            //{
+            //
+            //}
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i > 30) break;
+            }
+        }
+    }
+}
---
+++ TestProject/UnitTestOperaciones.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Xunit;
+using BankingLibrary;
+
+namespace TestProject
+{
+    public class UnitTestOperaciones
+    {
+        [Fact]
+        public void TestAperturaCuentas()
+        {
+            //Arrage
+            bool cuentaValida = false;
+
+            //Act
+            Operaciones.AperturaCuenta();
+            cuentaValida = Operaciones.abierta;
+            //LLamar al metodo de apertura de cuenta
+
+            //Assert
+            Assert.True(cuentaValida);
+            //Assert.True(Operaciones.abierta);
+        }
+
+        [Fact]
+        public void TestDeposito()
+        {
+            
+            Operaciones.AperturaCuenta();
+            Operaciones.Deposito(100);
+            Assert.Equal(100, Operaciones.saldo);
+        }
+
+        [Fact]
+        public void TestRetiro()
+        {
+            Operaciones.AperturaCuenta();
+            Operaciones.Deposito(100);
+            Operaciones.Retiro(20);
+            Assert.Equal(80, Operaciones.saldo);
+        }
+
+        [Fact]
+        public void TestRetiroSobregiroNopermitido()
+        {
+            //Verificar si el saldo se sobregiro
+            bool sobregiro = false;
+            try
+            {
+                Operaciones.AperturaCuenta();
+                Operaciones.Deposito(20);
+                Operaciones.Retiro(100);
+                if (Operaciones.saldo < 0)
+                    sobregiro = true;
+                Assert.False(sobregiro);
+            }
+            catch (Exception)
+            {
+                Assert.True(true);
+            }
+           
+        }
+
+        [Fact]
+        public void TestRetiroExcepcionporSobregiro()
+        {
+            Operaciones.AperturaCuenta();
+            Operaciones.Deposito(20);
+            Assert.Throws<ArgumentException>(()=>Operaciones.Retiro(100));
+
+        }
+
+        [Fact]
+        public void TestSaldo()
+        {
+            Operaciones.AperturaCuenta();
+            Operaciones.Deposito(100);
+            Operaciones.Retiro(100);
+            Assert.Equal(0, Operaciones.saldo);
+        }
+    }
+}
